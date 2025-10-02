149d461979c18f6158c4320dd424045d3282aaf3
Mindaugas <aiveris@gmail.com>
2025-10-02 18:19:58 +0300

    2025-10-02

--- js/script.js
+++ js/script.js
@@ -66,7 +66,7 @@ function add_player_2() {
 }
 function add_player_3() {
   db.collection("a1").add({
-    todo: "Simonas Šidlauskas 3",
+    todo: "Andrius Volkovas 3",
   });
 }
 function add_player_4() {
@@ -86,22 +86,22 @@ function add_player_7() {
 }
 function add_player_8() {
   db.collection("a1").add({
-    todo: "Laimonas Šegžda 8",
+    todo: "Vytautas Mereckis 8",
   });
 }
 function add_player_9() {
   db.collection("a1").add({
-    todo: "Arminas Zakarys 9",
+    todo: "Evaldas Stankevičius 9",
   });
 }
 function add_player_10() {
   db.collection("a1").add({
-    todo: "Edvinas Ščerbukas 10",
+    todo: "Donatas Bilius 10",
   });
 }
 function add_player_11() {
   db.collection("a1").add({
-    todo: "Karolis Bliodnieks 11",
+    todo: "Daivaras Lebednykas 11",
   });
 }
 function add_player_12() {
@@ -121,7 +121,7 @@ function add_player_14() {
 }
 function add_player_15() {
   db.collection("a1").add({
-    todo: "Artūras Simonaitis 15",
+    todo: "Mantas Kadzevičius 15",
   });
 }
 function add_player_19() {
@@ -131,7 +131,7 @@ function add_player_19() {
 }
 function add_player_23() {
   db.collection("a1").add({
-    todo: "Justas Sobolevskis 23",
+    todo: "Laurynas Čižius 23",
   });
 }
 function add_player_24() {
@@ -144,6 +144,11 @@ function add_player_27() {
     todo: "Mantas Kadzevičius 27",
   });
 }
+function add_player_29() {
+  db.collection("a1").add({
+    todo: "Tomas Dzeventauskas 29",
+  });
+}
 function add_player_30() {
   db.collection("a1").add({
     todo: "Mindaugas Beleka 30",
@@ -152,6 +157,11 @@ function add_player_30() {
 function add_player_33() {
   db.collection("a1").add({
     todo: "Rimantas Sagatas 33",
+  });
+}
+function add_player_35() {
+  db.collection("a1").add({
+    todo: "Tomas Purlys 35",
   });
 }
 function add_player_42() {
@@ -171,7 +181,7 @@ function add_player_69() {
 }
 function add_player_77() {
   db.collection("a1").add({
-    todo: "Donatas Bilius 77",
+    todo: "Rokas Židžiūnas 77",
   });
 }
 function add_player_82() {
@@ -187,6 +197,16 @@ function add_player_91() {
 function add_player_92() {
   db.collection("a1").add({
     todo: "Augustinas Stoškus 92",
+  });
+}
+function add_player_93() {
+  db.collection("a1").add({
+    todo: "Jonas Savickas 93",
+  });
+}
+function add_player_98() {
+  db.collection("a1").add({
+    todo: "Karolis Bliodnieks 98",
   });
 }
 function add_player_99() {
